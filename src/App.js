89a873d558c464c67198a15e3a50b25e3482e46d
anduser <>
2022-01-19 14:14:44 +0200

    add husky & lint-stage

--- src/App.js
+++ src/App.js
@@ -2,33 +2,36 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { initApp } from './store/app';
-import Layout from './components/Layout';
-import ToDo from './pages/ToDo';
+import Layout from "./components/Layout";
+import ToDo from "./pages/ToDo";
 import SignIn from './pages/SignIn';
 
-const Spinner = () => <span>Loading...</span>
+const Spinner = () => <span>Loading...</span>;
 
 const App = () => {
-    const isInit = useSelector(state => !!state?.app?.init)
-    const isAuth = useSelector(state => !!state?.auth?.token);
-    const dispatch = useDispatch();
+  const isInit = useSelector((state) => !!state?.app?.init);
+  const isAuth = useSelector((state) => !!state?.auth?.token);
+  const dispatch = useDispatch();
 
-    console.log('>>>', { isInit, isAuth })
+  console.log('>>>', { isInit, isAuth });
 
-    useEffect(() => {
-        dispatch(initApp());
-    }, []);
+  useEffect(() => {
+    dispatch(initApp());
+  }, []);
 
-    return !isInit
-        ? <Spinner/>
-        : (
-            <Layout>
-                <Routes>
-                    <Route path='/sign-in' element={<SignIn/>}/>
-                    <Route path='*' element={isAuth ? <ToDo/> : <Navigate to='/sign-in'/>}/>
-                </Routes>
-            </Layout>
-        );
-}
+  return !isInit ? (
+    <Spinner />
+  ) : (
+    <Layout>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route
+          path='*'
+          element={isAuth ? <ToDo /> : <Navigate to='/sign-in' />}
+        />
+      </Routes>
+    </Layout>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  unstable_HistoryRouter as HistoryRouter,
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -11,15 +14,12 @@ import App from './App';
 // How to use history in async action creator
 // https://github.com/remix-run/react-router/issues/8264#issuecomment-991271554
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <HistoryRouter history={history}>
-            {/*<BrowserRouter>*/}
-
-                <App/>
-            {/*</BrowserRouter>*/}
-            </HistoryRouter>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <HistoryRouter history={history}>
+        <App />
+      </HistoryRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
